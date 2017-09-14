4aaf11152bb1ad6bdf97823534d7b238f62cbdc4
Rafael Baptista <31322468+rafabaptista@users.noreply.github.com>
2017-09-13 23:30:13 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // ngModel lives here!!!
+import { RouterModule } from '@angular/router';
+
+import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component'
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service'
+import { DashboardComponent } from './dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+@NgModule({
+  imports:      [ BrowserModule,
+                  FormsModule, //import the FormsModule before binding with [(ngModel)]
+                  AppRoutingModule
+                ],
+  declarations: [ AppComponent,
+                  DashboardComponent,
+                  HeroDetailComponent,
+                  HeroesComponent ],
+  providers: [
+    HeroService
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
